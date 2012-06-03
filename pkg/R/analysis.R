7dad199ead810cb0d02b2dd033cd626dd32a6fd3
schwab <schwab@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-03 20:59:18 +0000

    Improved idt
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/emov@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/analysis.R
+++ pkg/R/analysis.R
@@ -3,6 +3,8 @@
 
 setwd("~/dev/emov/pkg/R")
 source("emov.R")
+# install.packages("circular")
+#library("circular")
 
 # read raw data file
 data = emov.read_iviewsamples(
@@ -18,15 +20,17 @@ data$L.GVEC.Y[data$L.GVEC.Y == 0] = NA
 data$L.GVEC.Z[data$L.GVEC.Z == 0] = NA
 
 # select channels to use
-data = data.frame(x = data$L.POR.X..mm, y = -data$L.POR.Y..mm)
+data = data.frame(t = data$Time, x = data$L.POR.X..mm, y = -data$L.POR.Y..mm)
 
-# filter data
-data = emov.filter(data$x, data$y, 10500/200)
+# filter data, 1 deg is 14.4 mm, filtering > 750 deg/s (10800 mm/s)
+flt = emov.filter(data$x, data$y, 10500/200)
+data$x = flt$x
+data$y = flt$y
+
+
 
 # cart2sphere
 #data = emov.cart2sphere(data$L.GVEC.X, data$L.GVEC.Y, data$L.GVEC.Z)
-# rad2deg and 
-#library("circular")
 #data = data.frame(x=deg(data$az), y=-deg(data$elev))
 
 # trial segmentation
@@ -41,13 +45,18 @@ idx <- matrix(idx, nrow=n, ncol=2, byrow=TRUE)
 idx <- data.frame(start=idx[,1], end=idx[,2]) # easy to access
 
 # fixation detecton for each trial
-disp  = 2.8*10 # in cm, 2.8 cm (2 deg)
-fix = list()
-for (i in 1:n) {
-  fix[[i]] = emov.idt(data$x[idx$start[i]:idx$end[i]],
-                      data$y[idx$start[i]:idx$end[i]],
-                      disp, 80/1000*200)
-}
+max_disp  = 19.0 # in cm, 28.8 cm (2 deg)
+min_dur =  80/1000*200
+fix = emov.idt(data$t, data$x, data$y, max_disp, min_dur)
+
+#fix = list()
+#for (i in 1:n) {
+#  fix[[i]] = emov.idt(data$x[idx$start[i]:idx$end[i]],
+                      #data$y[idx$start[i]:idx$end[i]],
+                      #disp, 80/1000*200)
+#}
+
+
 
 # Plot all trials, raw data and fixations
 #my_xlim = c(-35, 25)
@@ -69,7 +78,7 @@ for (i in 1:n) {
 par(mfcol=c(1,1))
 nr = 1
 plot(data$x[idx$start[nr]:idx$end[nr]],
-     data$y[idx$start[nr]:idx$end[nr]],
+     data$y[idx$start[nr]:idx$end[nr]],  
      type="l", xlim=my_xlim,  ylim=my_ylim,
      xlab="Horizontal (px)", ylab="Vertical (px)")
 par(new=TRUE)
@@ -78,25 +87,25 @@ plot(fix[[nr]]$x, fix[[nr]]$y, xlim=my_xlim,  ylim=my_ylim,
 
 # Plot stimuli
 # install.packages("jpeg")
-library(jpeg)
-
-img = list()
-img[[1]] <- readJPEG("/home/simon/Data/nscenes/stimuli/000.jpg")
-img[[2]] <- readJPEG("/home/simon/Data/nscenes/stimuli/001.jpg")
-img[[3]] <- readJPEG("/home/simon/Data/nscenes/stimuli/002.jpg")
-img[[4]] <- readJPEG("/home/simon/Data/nscenes/stimuli/003.jpg")
-img[[5]] <- readJPEG("/home/simon/Data/nscenes/stimuli/004.jpg")
-img[[6]] <- readJPEG("/home/simon/Data/nscenes/stimuli/005.jpg")
-img[[7]] <- readJPEG("/home/simon/Data/nscenes/stimuli/006.jpg")
-img[[8]] <- readJPEG("/home/simon/Data/nscenes/stimuli/007.jpg")
-img[[9]] <- readJPEG("/home/simon/Data/nscenes/stimuli/008.jpg")
-img[[10]] <- readJPEG("/home/simon/Data/nscenes/stimuli/009.jpg")
-img[[11]] <- readJPEG("/home/simon/Data/nscenes/stimuli/010.jpg")
-img[[12]] <- readJPEG("/home/simon/Data/nscenes/stimuli/011.jpg")
-
-par(mfcol=c(4,3))
-
-for (i in 1:n) {
-  plot(c(0,1), c(0,1))
-  rasterImage(img[[i]], 0, 0, 1, 1)
-}
+# library(jpeg)
+# 
+# img = list()
+# img[[1]] <- readJPEG("/home/simon/Data/nscenes/stimuli/000.jpg")
+# img[[2]] <- readJPEG("/home/simon/Data/nscenes/stimuli/001.jpg")
+# img[[3]] <- readJPEG("/home/simon/Data/nscenes/stimuli/002.jpg")
+# img[[4]] <- readJPEG("/home/simon/Data/nscenes/stimuli/003.jpg")
+# img[[5]] <- readJPEG("/home/simon/Data/nscenes/stimuli/004.jpg")
+# img[[6]] <- readJPEG("/home/simon/Data/nscenes/stimuli/005.jpg")
+# img[[7]] <- readJPEG("/home/simon/Data/nscenes/stimuli/006.jpg")
+# img[[8]] <- readJPEG("/home/simon/Data/nscenes/stimuli/007.jpg")
+# img[[9]] <- readJPEG("/home/simon/Data/nscenes/stimuli/008.jpg")
+# img[[10]] <- readJPEG("/home/simon/Data/nscenes/stimuli/009.jpg")
+# img[[11]] <- readJPEG("/home/simon/Data/nscenes/stimuli/010.jpg")
+# img[[12]] <- readJPEG("/home/simon/Data/nscenes/stimuli/011.jpg")
+# 
+# par(mfcol=c(4,3))
+# 
+# for (i in 1:n) {
+#   plot(c(0,1), c(0,1))
+#   rasterImage(img[[i]], 0, 0, 1, 1)
+# }
--- pkg/R/emov.R
+++ pkg/R/emov.R
@@ -8,64 +8,69 @@
 # Identifying fixations and saccades in eye-tracking protocols. In Proceedings
 # of the 2000 symposium on eye tracking research & applications (pp. 71-78).
 # New York: ACM.
-emov.idt <- function(x, y, dispersion, duration) {
 
-    # init variables
-    fix_start <- c()
-    fix_end <- c()
-    fix_x <- c()
-    fix_y <- c()
-
-    start <- 1 # window start position
-
-    while (start <= length(x) - duration) {
-
-        end <- start + duration - 1 # window end position
-        # create window        
-	x_win <- x[start:end]
-        y_win <- y[start:end]
-        # dispersion
-
-        D <- (max(x_win, na.rm=T) - min(x_win, na.rm=T)) + (max(y_win, na.rm=T) - min(y_win, na.rm=T))
-
-        j <- 1  # window expander
-
-        while (D <= dispersion & end + j <= length(x)) {
-            # expand window by 1 using j
-            x_win <- x[start:(end + j)]
-            y_win <- y[start:(end + j)]
-            D <- (max(x_win, na.rm=T) - min(x_win ,na.rm=T)) + (max(y_win ,na.rm=T) - min(y_win, na.rm=T))
-
-            if (D > dispersion) {
-                # select window as fixation
-                fix_start <- c(fix_start, start)
-                fix_end <- c(fix_end, end + j - 1)
-                fix_x <- c(fix_x, mean(x_win,na.rm=T))
-                fix_y <- c(fix_y, mean(y_win,na.rm=T))
-                start <- end + j - 1;  # skip window points
-                break
-            } else if (end + j == length(x)) {
-                # handle last window if data ends during a fixation
-                fix_start <- c(fix_start, start)
-                fix_end <- c(fix_end, end)
-                fix_x <- c(fix_x, mean(x_win,na.rm=T))
-                fix_y <- c(fix_y, mean(y_win,na.rm=T))
-                break
-            }            
-            j <- j + 1
-        }
-
-    start <- start + 1;
-
+# duration: nr of data points for minimal fix duration
+emov.idt <- function(t, x, y, dispersion, duration) {
+  
+  # init variables
+  fix_start <- c()
+  fix_end <- c()
+  fix_x <- c()
+  fix_y <- c()
+  
+  start <- 1 # window start position
+  
+  while (start <= length(x) - duration + 1) { # added + 1
+    # while: we move window by 1, if D > threshold
+    
+    end <- start + duration - 1 # window end position
+    # create window        
+    x_win <- x[start:end]
+    y_win <- y[start:end]
+    # dispersion    
+    D <- (max(x_win, na.rm=T) - min(x_win, na.rm=T)) + 
+      (max(y_win, na.rm=T) - min(y_win, na.rm=T))
+    
+    j <- 1  # window expander
+    
+    while (D <= dispersion & end + j <= length(x)) {
+      # while: we expand window by 1 using j
+      x_win <- x[start:(end + j)]
+      y_win <- y[start:(end + j)]
+      D <- (max(x_win, na.rm=T) - min(x_win ,na.rm=T))
+      + (max(y_win ,na.rm=T) - min(y_win, na.rm=T))
+      
+      if (D > dispersion) {
+        # select window (j - 1) as fixation
+        fix_start <- c(fix_start, t[start])
+        fix_end <- c(fix_end, t[end + j - 1]) # j - 1 is previous window
+        fix_x <- c(fix_x, mean(x_win, na.rm=T))
+        fix_y <- c(fix_y, mean(y_win, na.rm=T))
+        start <- end + j - 1 # skip window points
+        break # something wroing with -1
+      } else if (end + j == length(x)) {
+        # handle last window if data ends during a fixation
+        fix_start <- c(fix_start, t[start])
+        fix_end <- c(fix_end, t[end])
+        fix_x <- c(fix_x, mean(x_win,na.rm=T))
+        fix_y <- c(fix_y, mean(y_win,na.rm=T))
+        start <- end + j - 1
+        break
+      }            
+      j <- j + 1
     }
-
-    result <- data.frame(start=fix_start,
-                         end=fix_end,
-                         x=fix_x,
-                         y=fix_y,
-                         dur=fix_end-fix_start)
-    return(result)
-
+    
+    start <- start + 1;
+    
+  }
+  
+  result <- data.frame(start=fix_start,
+                       end=fix_end,
+                       dur=fix_end-fix_start,
+                       x=fix_x,
+                       y=fix_y)
+  return(result)
+  
 }
 
 emov.read_iviewsamples <- function(file, nr_of_headerlines) {
